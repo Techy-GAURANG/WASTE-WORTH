50b262dce8912aa05b21d3f02a27b3d8488ba4e3
Techy-GAURANG <PRITHVISM2121@GMAIL.COM>
2026-05-04 20:47:56 +0530

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -12,7 +12,7 @@ document.head.appendChild(meta);
 // Register service worker
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker
+    navigator.serviceWorker.register("/WASTE-WORTH/service-worker.js")
       .register("/service-worker.js")
       .then(() => console.log("Service Worker Registered"))
       .catch(err => console.log("SW error:", err));
